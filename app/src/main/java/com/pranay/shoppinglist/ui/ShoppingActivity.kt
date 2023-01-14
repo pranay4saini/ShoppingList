03a9c7579fbcf431675b547bedd01129eaa0eec0
pranay <prana4saini@gmail.com>
2023-01-14 16:46:58 +0530

    added explosion animation to the floating action button

--- app/src/main/java/com/pranay/shoppinglist/ui/ShoppingActivity.kt
+++ app/src/main/java/com/pranay/shoppinglist/ui/ShoppingActivity.kt
@@ -2,9 +2,14 @@ package com.pranay.shoppinglist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AnimationUtils
+
+import androidx.core.view.isVisible
+
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.pranay.shoppinglist.R
 
 import com.pranay.shoppinglist.data.db.entities.ShoppingItem
 
@@ -17,6 +22,7 @@ import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.kodein
 import org.kodein.di.generic.instance
+
 
 
 class ShoppingActivity : AppCompatActivity(), KodeinAware {
@@ -33,6 +39,11 @@ class ShoppingActivity : AppCompatActivity(), KodeinAware {
         val view = binding.root
         setContentView(view)
 
+        val animation = AnimationUtils.loadAnimation(this, R.anim.circle_explosion_anim).apply {
+            duration = 700
+            interpolator  = AccelerateDecelerateInterpolator()
+        }
+
 
         val viewModel = ViewModelProviders.of(this,factory).get(ShoppingViewModel::class.java)
         val adapter = ShoppingItemAdapter(listOf(),viewModel)
@@ -45,12 +56,19 @@ class ShoppingActivity : AppCompatActivity(), KodeinAware {
         })
 
         binding.fab.setOnClickListener {
-            AddShoppingItem(this,
-            object : AddDialogListner{
-                override fun onAddButtonClicked(item: ShoppingItem) {
-                    viewModel.upsert(item)
-                }
-            }).show()
+            binding.fab.isVisible = false
+            binding.circle.isVisible = true
+            binding.circle.startAnimation(animation) {
+                AddShoppingItem(this,
+                    object : AddDialogListner{
+                        override fun onAddButtonClicked(item: ShoppingItem) {
+                            viewModel.upsert(item)
+                        }
+                    }).show()
+                binding.circle.isVisible = false
+                binding.fab.isVisible = true
+            }
+
         }
 
     }
